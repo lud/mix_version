8a7b9906828bb34de01e0072bbeaac3318456972
lud <ludovic@demblans.com>
2024-03-11 23:37:45 +0100

    fix: Fixed --anotate that would always default to false (upgrade of cli_mate)

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule MixVersion.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:cli_mate, "~> 0.1.1", runtime: false}
+      {:cli_mate, "~> 0.3", runtime: false}
     ]
   end
 
