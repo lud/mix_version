95c75c06d2bf47a3dadbd9cf129dc14784751c26
lud <ludovic@demblans.com>
2025-04-23 01:04:44 +0200

    (fix deps)

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule MixVersion.MixProject do
       # Dev, Test
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:ex_check, "~> 0.16.0", only: [:dev, :test]},
+      {:ex_check, "~> 0.16.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: :test, runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test]}
     ]
